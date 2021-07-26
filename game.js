5a07e76f73f4b0b29cd968fb6e6548b63eecf103
Rodney Dev <83191081+rodevrel@users.noreply.github.com>
2021-07-25 21:17:27 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ Crafty.init(500,350, document.getElementById('game'));
 
 var player = Crafty.e('2D, Canvas, Color, Twoway, Gravity').attr({x: 0, y: 0, w: 50, h: 50}).color('green').twoway(200).gravity('Floor');
 
-var f1 = Crafty.e('2D, Color, Floor').attr({x: -100, y: 5000, w: 1000, h: 100}).color('#F5F5DC')
+var f1 = Crafty.e('2D, Color, Floor').attr({x: 0, 5000, w: 5000 100}).color('#F5F5DC')
 
 
 
