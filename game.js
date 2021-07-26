3d02f96ecf3a514cf53f222b0ab80de4b01295ba
Rodney Dev <83191081+rodevrel@users.noreply.github.com>
2021-07-25 21:18:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,13 @@ Crafty.init(500,350, document.getElementById('game'));
 
 var player = Crafty.e('2D, Canvas, Color, Twoway, Gravity').attr({x: 0, y: 0, w: 50, h: 50}).color('green').twoway(200).gravity('Floor');
 
-var f1 = Crafty.e('2D, Color, Floor').attr({x: 0, 5000, w: 5000 100}).color('#F5F5DC')
+Crafty.e('2D, Color, Floor').attr({x: 0, y:5000, w: 5000 100}).color('black')
 
 
 
 
 
-Crafty.viewport.clampToEntities = false;
+Crafty.viewport.clampToEntities = true;
 Crafty.viewport.scale(1.5);
 Crafty.one("CameraAnimationDone", function() {
     Crafty.viewport.follow(player, 0, 0);
